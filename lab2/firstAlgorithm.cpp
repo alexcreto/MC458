91dd62fab725ea761d5818b7a0805ab268bb2409
italomg <italogmoraes@gmail.com>
2014-10-25 15:28:22 -0200

    Agora estamos pegando de volta os elementos do conjunto idependente

--- lab2/firstAlgorithm.cpp
+++ lab2/firstAlgorithm.cpp
@@ -7,77 +7,93 @@ typedef unsigned int uint;
 using namespace std;
 
 vector<vector<uint> > insertion_sort(vector<vector<uint> > M, int n, uint *ordem[]) {
-	uint i, j , a[n], aux;
-	for (i = 1; i < n; i++) {
-		j = i;
-		while (j > 0 && M[j - 1][0] > M[j][0]) {
-			M[j].swap(M[j - 1]);
-			for(uint i = 0; i < n; i++){
-				a[i] = M[i][j];
-				M[i][j] = M[i][j-1];
-				M[i][j-1] = a[i];
-				aux = *ordem[j-1];
-				*ordem[j-1] = *ordem[j];
-				*ordem[j] = aux;
-			}
+  uint i, j , a[n], aux;
+  for (i = 1; i < n; i++) {
+    j = i;
+    while (j > 0 && M[j - 1][0] > M[j][0]) {
+      M[j].swap(M[j - 1]);
+      for(uint i = 0; i < n; i++){
+	a[i] = M[i][j];
+	M[i][j] = M[i][j-1];
+	M[i][j-1] = a[i];
+	//aux = *ordem[j-1];
+	//*ordem[j-1] = *ordem[j];
+	//*ordem[j] = aux;
+      }
+      j--;
+    }
+  }
+  return M;
+}
 
-			j--;
-		}
+void ajusta_ci(vector<vector<uint> > M, uint *seq_vert_ci){
+  //Estou usando M.size() pq M sempre eh quadrada
+  //Primeiro vou zerar todos os vertices ligados
+  //a este que eu quero adicionar nas respostas
+  for(uint i = 0; i < M.size(); i++){
+    //tanto faz se o i eh linha ou coluna
+    //a matriz eh sempre simetrica
+    if(seq_vert_ci[i] == 1){
+        for(uint j = 0; j < M.size(); j++){
+	  //se o cara esta na resposta e esta ligado
+	  //com outro da resposta desligue o outro da
+	  //resposta
+	  if(M[i][j])
+	    seq_vert_ci[j] = 0;
 	}
-	return M;
+    }
+  }
 }
 
-int calculaConjuntoIndependente(vector<vector<uint> > graph, uint vertices){
+uint A1(uint n, vector<vector<uint> > M, uint *tam_ci, uint *seq_vert_ci, uint tempo_maximo, uint *ordem){
+  /*
+   * n 			=> 	Numero de vertices
+   * M 			=> 	Matriz de adjacencia
+   * tam_ci 		=> 	Tamanho do Conj. Ind.
+   * seq_vert_ci 	=> 	Vertices do CI
+   * tempo_maximo => 	Limite de tempo, em segundos, para um algoritmo encontrar o CI
+   */
+
+  if(n <= 0) return 0;
+  uint G0, G1;
+  uint *proxOrdem;
+
+  // Usando M[0][0] como nosso v:
+  // M0: v nao esta na solucao	
+  vector<vector<uint> >M0(n-1, vector<uint>(n-1,0));	
+	
+  for(uint i = 0; i < n-1; i++){	//Pega apenas as linhas e colunas que nao sao de v
+    for(uint j = 0; j < n-1; j++)
+      M0[i][j] = M[i+1][j+1];
+  }
+	
+  //passa o vetor ordem como se comecasse do proximo elemento
+  proxOrdem = &(ordem[1]);
+  //Chamar recursao M0
+  G0 = A1(n-1, M0, tam_ci, seq_vert_ci, tempo_maximo, proxOrdem);
+	
+	
+  // M1: v esta na solucao
+	
+  uint adj = 0;						// Numero de vertices adjacentes a v
+  for(uint i = 0; i < n; i++) if(M[i][0]) adj++;
+  M = insertion_sort(M, n, &ordem);	// Mexe na matriz de forma que 
+  // os adj se acumulam no fundo
   
+  vector<vector<uint> >M1(n-1-adj, vector<uint>(n-1-adj,0));
+  for(uint i = 0; i < n-1-adj; i++){	//Pega apenas as linhas e colunas que nao sao de v e nem dos adj
+    for(uint j = 0; j < n-1-adj; j++)
+      M1[i][j] = M[i+1][j+1];
+  }
+  //TODO jogar o v do topo de ordem em *seq_vert_ci; depois tam_ci++;
+  //Chamar recursao M1
+  G1 = A1(n-1-adj, M1, tam_ci, seq_vert_ci, tempo_maximo, proxOrdem);
   
-
-  return 0;
-}
-
-uint A1(uint n, vector<vector<uint> > M, uint *tam_ci, uint *seq_vert_ci, uint tempo_maximo, uint ordem[]){
-	/*
-	 * n 			=> 	Numero de vertices
-	 * M 			=> 	Matriz de adjacencia
-	 * tam_ci 		=> 	Tamanho do Conj. Ind.
-	 * seq_vert_ci 	=> 	Vertices do CI
-	 * tempo_maximo => 	Limite de tempo, em segundos, para um algoritmo encontrar o CI
-	 */
-
-	if(n <= 0) return 0;
-	uint G0, G1;
-	
-	// Usando M[0][0] como nosso v:
-	// M0: v nao esta na solucao	
-	vector<vector<uint> >M0(n-1, vector<uint>(n-1,0));	
-	
-	for(uint i = 0; i < n-1; i++){	//Pega apenas as linhas e colunas que nao sao de v
-      for(uint j = 0; j < n-1; j++)
-		M0[i][j] = M[i+1][j+1];
-    }
-	
-	//Chamar recursao M0
-	G0 = A1(n-1, M0, tam_ci, seq_vert_ci, tempo_maximo, ordem);
-	
-	
-	// M1: v esta na solucao
-	
-	uint adj = 0;						// Numero de vertices adjacentes a v
-	for(uint i = 0; i < n; i++) if(M[i][0]) adj++;
-	M = insertion_sort(M, n, &ordem);	// Mexe na matriz de forma que 
-										// os adj se acumulam no fundo
-	
-	vector<vector<uint> >M1(n-1-adj, vector<uint>(n-1-adj,0));
-	for(uint i = 0; i < n-1-adj; i++){	//Pega apenas as linhas e colunas que nao sao de v e nem dos adj
-      for(uint j = 0; j < n-1-adj; j++)
-		M1[i][j] = M[i+1][j+1];
-    }
-//TODO jogar o v do topo de ordem em *seq_vert_ci; depois tam_ci++;
-	
-	//Chamar recursao M1
-	G1 = A1(n-1-adj, M1, tam_ci, seq_vert_ci, tempo_maximo, ordem);
-	
-	if (G0 > G1) return G0;
-	else return 1+G1;
+  if (G0 > G1)return G0;
+  else{
+    seq_vert_ci[ordem[0]] = 1;
+    return 1+G1;
+  }
 }
 
 uint A2(uint n, vector<vector<uint> > M, uint *tam_ci, uint *seq_vert_ci, uint tempo_maximo, uint ordem[]); 	// A2 do enunciado
@@ -92,23 +108,27 @@ int main (){
   vector<vector<uint> >graph(nmrDeVertices, vector<uint>(nmrDeVertices,0));
 
   while(nmrDeGrafos--){
-	uint ordem[nmrDeVertices];
-		
+    uint ordem[nmrDeVertices], seq_vert_ci[nmrDeVertices];
+	
     for(uint arestas = 0; arestas < nmrDeArestas; arestas++){
       cin>>vertice>>otoVertice;
       graph[vertice][otoVertice] = 1;
       graph[otoVertice][vertice] = 1;
     }
     for(uint i = 0; i < nmrDeVertices; i++){
-		for(uint j = 0; j < nmrDeVertices; j++){
-			cout<<graph[i][j];
-			ordem[i] = i;
-		}
+      for(uint j = 0; j < nmrDeVertices; j++){
+	cout<<graph[i][j];
+	ordem[i] = i;
+	seq_vert_ci[i] = 0;
+      }
       cout<<endl;
     }
     cout<<endl;
-	  
-	cout<<A1(nmrDeVertices, graph, 0,0,0, ordem);
+
+    cout<<A1(nmrDeVertices, graph, 0,(uint *)&seq_vert_ci,0, (uint *)&ordem)<<endl<<endl;
+    ajusta_ci(graph, (uint *)&seq_vert_ci);
+    for(uint i = 0; i < nmrDeVertices; i++)
+      cout<<seq_vert_ci[i]<<" ";
   }
   
   return 0;
