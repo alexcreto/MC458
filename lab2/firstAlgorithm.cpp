f37216a60802eca97979f6f747e90865cad88e0c
italomg <italogmoraes@gmail.com>
2014-10-25 18:29:13 -0200

    sem muitas mudancas, mas progredindo

--- lab2/firstAlgorithm.cpp
+++ lab2/firstAlgorithm.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
-#include<vector>
-#include<stdio.h>
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
 
 typedef unsigned int uint;
 
@@ -50,8 +51,8 @@ void ajusta_ci(vector<vector<uint> > M, uint *seq_vert_ci){
  * no A2, ai ficava meio complicado pra acular tds no final, pq tb
  * vamos precisar rastrear qual o grau deles.
  */
-vector<vector<uint> > excluiVertice(vector<vector<uint> > M, uint vertice){
-  //  M0 = M;
+vector<vector<uint> > excluiVertice(vector<vector<uint> > M, uint vertice, vector<uint> *grau){
+  grau->erase(grau->begin()+vertice);
   M.erase(M.begin()+vertice);
   uint size = M.size();
   for(uint i = 0; i < size; i++){
@@ -60,18 +61,22 @@ vector<vector<uint> > excluiVertice(vector<vector<uint> > M, uint vertice){
   return M;
 }
 
-vector<vector<uint> > excluiAdjacentes(vector<vector<uint> > M, uint vertice){
+vector<vector<uint> > excluiAdjacentes(vector<vector<uint> > M, uint vertice, vector<uint> *grau){
   vector<vector<uint> > M1 = M;
   uint size = M.size();
   for(uint i = 0; i < size; i++){
     if(M[i][vertice])
-      M1 = excluiVertice(M,i);
+      M1 = excluiVertice(M,i,grau);
   }
-  M1 = excluiVertice(M,vertice);;
+  M1 = excluiVertice(M,vertice,grau);
   return M1;
 }
 
-uint A1(uint n, vector<vector<uint> > M, uint *tam_ci, uint *seq_vert_ci, uint tempo_maximo, uint *ordem){
+uint maxOfAll(uint *maxList){
+
+}
+
+uint A1(uint n, vector<vector<uint> > M, vector<uint> *grau, uint *seq_vert_ci, uint tempo_maximo, uint *ordem){
   /*
    * n 			=> 	Numero de vertices
    * M 			=> 	Matriz de adjacencia
@@ -92,12 +97,12 @@ uint A1(uint n, vector<vector<uint> > M, uint *tam_ci, uint *seq_vert_ci, uint t
   //   for(uint j = 0; j < n-1; j++)
   //     M0[i][j] = M[i+1][j+1];
   // }
-  M0 = excluiVertice(M, 0);
+  M0 = excluiVertice(M, 0, grau);
 	
   //passa o vetor ordem como se comecasse do proximo elemento
   proxOrdem = &(ordem[1]);
   //Chamar recursao M0
-  G0 = A1(n-1, M0, tam_ci, seq_vert_ci, tempo_maximo, proxOrdem);
+  G0 = A1(n-1, M0, grau, seq_vert_ci, tempo_maximo, proxOrdem);
 	
 	
   // M1: v esta na solucao
@@ -112,19 +117,21 @@ uint A1(uint n, vector<vector<uint> > M, uint *tam_ci, uint *seq_vert_ci, uint t
   //   for(uint j = 0; j < n-1-adj; j++)
   //     M1[i][j] = M[i+1][j+1];
   // }
-  M1 = excluiAdjacentes(M,0);
+  M1 = excluiAdjacentes(M,0,grau);
   //TODO jogar o v do topo de ordem em *seq_vert_ci; depois tam_ci++;
   //Chamar recursao M1
-  G1 = A1(n-1-adj, M1, tam_ci, seq_vert_ci, tempo_maximo, proxOrdem);
+  G1 = A1(n-1-adj, M1, grau, seq_vert_ci, tempo_maximo, proxOrdem)+1;
   //pelo q eu entendi tem q comparar G0 com G1+1, mas nao tenho crtz
-  if (G0 > (G1+1))return G0;
+  if (G0 > G1)return G0;
   else{
     seq_vert_ci[ordem[0]] = 1;
-    return 1+G1;
+    return G1;
   }
 }
 
-uint A2(uint n, vector<vector<uint> > M, uint *grau, uint *seq_vert_ci, uint tempo_maximo, uint ordem[]){// A2 do enunciado
+
+
+uint A2(uint n, vector<vector<uint> > M, vector<uint> grau, uint *seq_vert_ci, uint tempo_maximo, uint ordem[]){// A2 do enunciado
     /*
    * n 			=> 	Numero de vertices
    * M 			=> 	Matriz de adjacencia
@@ -136,56 +143,44 @@ uint A2(uint n, vector<vector<uint> > M, uint *grau, uint *seq_vert_ci, uint tem
   if(n <= 0) return 0;
   uint GN, G0, G1, G3;
   uint *proxOrdem;
-
-  // Usando M[0][0] como nosso v:
-  // M0: v nao esta na solucao	
-	
-  // for(uint i = 0; i < n-1; i++){	//Pega apenas as linhas e colunas que nao sao de v
-  //   for(uint j = 0; j < n-1; j++)
-  //     M0[i][j] = M[i+1][j+1];
-  // }
-  vector<vector<uint> >MN(n-1, vector<uint>(n-1,0));
-  MN = excluiVertice(M, 0);
+  vector<uint> modGrau = grau;
   //passa o vetor ordem como se comecasse do proximo elemento
   proxOrdem = &(ordem[1]);
-  //Chamar recursao M0
 
-	
-	
-  // M1: v esta na solucao
-	
-  uint adj = 0;						// Numero de vertices adjacentes a v
+  uint adj = 0; // Numero de vertices adjacentes a v
   for(uint i = 0; i < n; i++) if(M[i][0]) adj++;
-  // M = insertion_sort(M, n, &ordem);	// Mexe na matriz de forma que 
-  // // os adj se acumulam no fundo
-  
 
-  // for(uint i = 0; i < n-1-adj; i++){	//Pega apenas as linhas e colunas que nao sao de v e nem dos adj
-  //   for(uint j = 0; j < n-1-adj; j++)
-  //     M1[i][j] = M[i+1][j+1];
-  // }
-  vector<vector<uint> >M3(n-1-adj, vector<uint>(n-1-adj,0));
-  M3 = excluiAdjacentes(M,0);
-  //TODO jogar o v do topo de ordem em *seq_vert_ci; depois tam_ci++;
-  //Chamar recursao M1
-
+  //Cria copias pros varios possiveis grafos
+  vector<vector<uint> >MN(n-1, vector<uint>(n-1,0));
   vector<vector<uint> >M0(n-1, vector<uint>(n-1-adj,0));
   vector<vector<uint> >M1(n-2, vector<uint>(n-1-adj,0));
+  vector<vector<uint> >M3(n-1-adj, vector<uint>(n-1-adj,0));
+
   for(uint k = 0; k < n; k++){
-    if(grau[k] == 0)
-      G0 = A2(n-1, M0, grau, seq_vert_ci, tempo_maximo, proxOrdem);
-    else if(grau[k] == 1)
-      G1 = A2(n-2, M1, grau, seq_vert_ci, tempo_maximo, proxOrdem);
+    //caso o grau seja 0, trivialmente ele estara na resposta
+    if(grau[k] == 0){
+      MN = excluiVertice(M, k, &grau);
+      G0 = A2(n-1, M0, grau, seq_vert_ci, tempo_maximo, proxOrdem) + 1;
+    }
+    //caso grau seja 1, ele tb estara na resposta
+    else if(grau[k] == 1){
+      M3 = excluiAdjacentes(M, k, &grau);
+      G1 = A2(n-2, M1, grau, seq_vert_ci, tempo_maximo, proxOrdem) + 1;
+    }
+    //de resto continua td igual
     else{
+      MN = excluiVertice(M, k, &grau);
       GN = A2(n-1, MN, grau, seq_vert_ci, tempo_maximo, proxOrdem);
-      G3 = A2(n-1-adj, M3, grau, seq_vert_ci, tempo_maximo, proxOrdem);
+
+      M3 = excluiAdjacentes(M, k, &modGrau);
+      G3 = A2(n-1-adj, M3, grau, seq_vert_ci, tempo_maximo, proxOrdem) + 1;
     }
   }
   
   if (GN > G3)return GN;
   else{
     seq_vert_ci[ordem[0]] = 1;
-    return 1+G3;
+    return G3;
   }
 }
 uint A3(uint n, vector<vector<uint> > M, uint *tam_ci, uint *seq_vert_ci, uint tempo_maximo, uint ordem[]);		// Algoritmo que devemos bolar que supere A1 e A2
@@ -200,20 +195,25 @@ int main (){
 
   while(nmrDeGrafos--){
     uint ordem[nmrDeVertices], seq_vert_ci[nmrDeVertices];
-	
+    vector<uint> grau(nmrDeVertices,0);
+
+    for(uint i = 0; i < nmrDeVertices; i++){
+      //for(uint j = 0; j < nmrDeVertices; j++){
+      //cout<<graph[i][j];
+      ordem[i] = i;
+      seq_vert_ci[i] = 0;
+      //}
+      //cout<<endl;
+    }	
+    
     for(uint arestas = 0; arestas < nmrDeArestas; arestas++){
       cin>>vertice>>otoVertice;
       graph[vertice][otoVertice] = 1;
       graph[otoVertice][vertice] = 1;
+      grau[vertice] += 1;
+      grau[otoVertice] += 1;
     }
-    for(uint i = 0; i < nmrDeVertices; i++){
-      //for(uint j = 0; j < nmrDeVertices; j++){
-	//cout<<graph[i][j];
-	ordem[i] = i;
-	seq_vert_ci[i] = 0;
-	//}
-      //cout<<endl;
-    }
+    
     //cout<<endl;
 
     cout<<A1(nmrDeVertices, graph, 0,(uint *)&seq_vert_ci,0, (uint *)&ordem)<<endl<<endl;
